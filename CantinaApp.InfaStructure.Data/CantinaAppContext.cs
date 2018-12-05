b5d99c154adc64edb9ba523bd90a98f825d9716d
Sub-Z3r0 <andersen19@gmail.com>
2018-12-05 12:46:56 +0100

    test

--- CantinaApp.InfaStructure.Data/CantinaAppContext.cs
+++ CantinaApp.InfaStructure.Data/CantinaAppContext.cs
@@ -16,7 +16,31 @@ namespace CantinaApp.InfaStructure.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
- 
+            //Main food can have many ingredients
+            modelBuilder.Entity<Ingredients>()
+                .HasOne(p => p.MainFoodType)
+                .WithMany(s => s.IngredientsType)
+                .OnDelete(DeleteBehavior.SetNull);
+            //Main Food can have many allergens
+            modelBuilder.Entity<Allergen>()
+                .HasOne(a => a.MainFoodType)
+                .WithMany(a => a.AllergensType)
+                .OnDelete(DeleteBehavior.SetNull);
+            //Main Food can only have one Icon
+            modelBuilder.Entity<MainFood>()
+                .HasOne(a => a.FoodIconType);
+            //Ingredientsd can only have one Icon
+            modelBuilder.Entity<Ingredients>()
+                .HasOne(a => a.FoodIconType);
+            //Special Offers can only have one Icon
+            modelBuilder.Entity<SpecialOffers>()
+                .HasOne(a => a.FoodIconType);
+            //AllergensType can only have one Icon
+            modelBuilder.Entity<Allergen>()
+                .HasOne(a => a.FoodIconType);
+            //Main food has PK ID and IngredientsType
+            modelBuilder.Entity<MainFood>()
+                .HasKey(m => new { m.Id });
         }
         //Tables
         public DbSet<Users> User { get; set; }
