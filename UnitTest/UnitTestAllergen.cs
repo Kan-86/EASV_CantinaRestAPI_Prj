c23cf11e5760452737f6be375182238ba817ec64
liverpoljer <liverpoljer@live.dk>
2018-12-06 10:43:12 +0100

    test

---
+++ UnitTest/UnitTestAllergen.cs
@@ -0,0 +1,39 @@
+﻿using CantinaApp.Core.ApplicationServices;
+using CantinaApp.Core.ApplicationServices.Services;
+using CantinaApp.Core.DomainServices;
+using CantinaApp.Core.Entity.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTest
+{
+    class UnitTestAllergen
+    {
+        Mock<IAllergensRepositories> AllergensRepo;
+        IAllergenServices AllergensService;
+        public UnitTestAllergen()
+        {
+            AllergensRepo = new Mock<IAllergensRepositories>();
+            AllergensService = new AllergenServices(AllergensRepo.Object);
+        }
+
+        [TestMethod]
+        public void createAllergenWithoutNew()
+        {
+            var allergen = new Allergen()
+            {
+                FoodIconType = new FoodIcon()
+                {
+                    FoodIconType = "hey"
+                },
+                
+
+
+            };
+
+        }
+    }
+}
--- UnitTest/UnitTestMOTD.cs
+++ UnitTest/UnitTestMOTD.cs
@@ -1,20 +1,47 @@
 using CantinaApp.Core.ApplicationServices;
+using CantinaApp.Core.ApplicationServices.Services;
 using CantinaApp.Core.DomainServices;
-using CantinaApp.InfaStructure.Data;
-using CantinaApp.InfaStructure.Data.SQLRepositories;
-using Microsoft.EntityFrameworkCore;
+using CantinaApp.Core.Entity.Entities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.IO;
 
 namespace UnitTest
 {
     [TestClass]
     public class UnitTestMOTD
     {
-        [TestMethod]
-        public void  MOTDWithNoTip()
+        Mock<IMOTDRepositories> MotdRepo;
+        IMOTDServices motdService;
+        public UnitTestMOTD()
         {
-            
+             MotdRepo = new Mock<IMOTDRepositories>();
+            motdService = new MOTDServices(MotdRepo.Object);
+        }
 
+        [TestMethod]
+        public void CreateMOTDWithoutMessage()
+        {
+            var motd = new MOTD()
+            {
+
+            };
+
+            Exception ex = Assert.ThrowsException<ArgumentException>(() =>
+                motdService.AddMOTD(motd));
+            Assert.AreEqual("You need to write a message", ex.Message);
+        }
+
+        [TestMethod]
+        public void CreateMOTDWithName()
+        {
+            var motd = new MOTD()
+            {
+                TipOfTheDay= "One Dane per day, makes problems go away"
+            };
+
+            motdService.AddMOTD(motd);
         }
     }
 }
