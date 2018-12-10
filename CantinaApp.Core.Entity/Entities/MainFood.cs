b82187ce45ae02e74ae45abe0a33e304780faff0
SamPerCaparros <samu1667@easv365.dk>
2018-12-10 12:24:30 +0100

    Context and allergen table

--- CantinaApp.Core.Entity/Entities/MainFood.cs
+++ CantinaApp.Core.Entity/Entities/MainFood.cs
@@ -9,7 +9,7 @@ namespace CantinaApp.Core.Entity.Entities
         public int Id { get; set; }
         public String MainFoodName { get; set; }
         public List<RecipeLine> RecipeLines { get; set; }
-        public List<Allergens> AllergensTypeId { get; set; }
+        public List<AllergensInMenu> AllergensInMenu { get; set; }
         public DateTime FoodDate { get; set; }
         public int FoodIconId { get; set; }
     }
