237e483fb7ed696934bda1db55354af381c45376
SamPerCaparros <samu1667@easv365.dk>
2018-12-12 12:06:59 +0100

    Update Startup.cs

--- EASV_Cantina/Startup.cs
+++ EASV_Cantina/Startup.cs
@@ -159,7 +159,7 @@ namespace EASV_Cantina
                     var services = scope.ServiceProvider;
                     var dbContext = services.GetService<CantinaAppContext>();
                     dbContext.Database.EnsureCreated();
-                    DBInitializer.SeedDb(dbContext);
+                    //DBInitializer.SeedDb(dbContext);
                 }
                 app.UseHsts();
             }
